f30592249295cbaa06e8b00679e96418fdf11060
Noah Greenberg <noah@z98.co>
2024-08-29 23:05:14 -0400

    Revert to 4 player HUD if 5 and 6 are inactive

--- asm/css_exit.asm
+++ asm/css_exit.asm
@@ -10,6 +10,13 @@
     cmpi 0, r3, 2
     bne return
 
+    # Reset the HUD to the default 4 players.
+    # The loop below will expand it if this match requires it.
+    lis r4, 0x8048
+    lbz r5, 0x7C0(r4)
+    andi. r5, r5, 0xF7 # Mask off 8.
+    stb r5, 0x7C0(r4)
+
 .set player_size, 0x24
 
 .set pad_size, 0x44
@@ -32,6 +39,7 @@ loop:
     lbz r4, pad_offset_error(reg_pad_data)
     cmpli 0, r4, pad_err_ok
     bne load_none
+    # Load the player data to copy.
     lwz r4, 0(reg_player_data)
     lwz r5, 7(reg_player_data)
     b store
@@ -43,6 +51,16 @@ store:
     # Offsets 0 and 7 but +0x90 (player_size * 4).
     stw r4, 0x90(reg_player_data)
     stw r5, 0x97(reg_player_data)
+    # If the player being stored is active, expand the HUD to 6 slots.
+    # We check this instead of the controller status because p5/p6 may not be playing.
+    lbz r4, 0x91(reg_player_data)
+    cmpli 0, r4, 3 # 3=NONE.
+    beq control
+    # Expand the HUD to 6 slots.
+    lis r4, 0x8048
+    lbz r5, 0x7C0(r4)
+    ori r5, r5, 8 # Bitmask for 6 players.
+    stb r5, 0x7C0(r4)
 control:
     addi reg_player_data, reg_player_data, player_size
     addi reg_pad_data, reg_pad_data, pad_size
